2d9cc83a5143b688356911f5c0110e45261013c2
Diego Souza <diego@stallos.com.br>
2020-05-01 19:36:08 -0300

    alternado ServiceGatewayFactory

--- Payment.Service/Services/Factories/ServiceGatewayFactory.cs
+++ Payment.Service/Services/Factories/ServiceGatewayFactory.cs
@@ -4,18 +4,20 @@ using System.Reflection;
 using Payment.Infrastructure.Entities;
 using Payment.Infrastructure.CustomAttributes;
 using Payment.Service.Services.Gateways.Interfaces;
+using System.Collections.Generic;
 
 namespace Payment.Service.Services.Factories
 {
     internal static class ServiceGatewayFactory
     {
+        private static readonly List<TypeInfo> _definedTypes =
+            Assembly.GetExecutingAssembly().DefinedTypes
+                    .Where(t => t.CustomAttributes.Any(c => c.AttributeType == typeof(ServiceGatewayAttribute)))
+                    .ToList();
+
         public static IServiceGateway ServiceGateway(TipoPagamento tipoPagamento)
         {
-            var definedTypes = Assembly.GetExecutingAssembly().DefinedTypes
-                                       .Where(t => t.CustomAttributes.Any(c => c.AttributeType == typeof(ServiceGatewayAttribute)))
-                                       .ToList();
-
-            var typeInfoClass = definedTypes.Find(
+            var typeInfoClass = _definedTypes.Find(
                 t => t.CustomAttributes.Any(c => c.NamedArguments.Any(t => (TipoPagamento)t.TypedValue.Value == tipoPagamento))
             );
 
